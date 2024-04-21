2aab832b65915786f4b68f7fa44cd6381e0bb39f
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-21 20:40:35 -0300

    new associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
   has_many :created_events, class_name: 'Event', foreign_key: 'user_id'
+  has_many :attended_events, foreign_key: 'participant_id'
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
