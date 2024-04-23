edf6b7fd299245c09da0328badb1ac611e221b29
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-23 09:27:54 -0300

    new associations added

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  belongs_to :participant, class_name: 'User'
+  has_many :event_participants
+  has_many :participants, through: :event_participants, source: :user
+
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :future, -> { where('date >= ?', Date.today) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-
   has_many :created_events, class_name: 'Event', foreign_key: 'user_id'
-  has_many :attended_events, foreign_key: 'participant_id'
+  has_many :event_participants, foreign_key: 'participant_id'
+  has_many :attended_events, through: :event_participants, source: :event
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
