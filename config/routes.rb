eb4b5394507287ce4b8816925fd14c9b791a0e8b
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-23 09:29:55 -0300

    defined new root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get '/users/:id', to: 'users#show', as: 'user'
 
+  root "events#index"
   resources :events, only:[:index, :new, :create, :show]
 
   resources :events do
