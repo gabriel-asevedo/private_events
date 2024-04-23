a621ea253bbb40e0e7d25fe6004954c0f42916df
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-04-23 10:38:43 -0300

    references added

--- db/migrate/20240421234157_create_event_participants.rb
+++ db/migrate/20240421234157_create_event_participants.rb
@@ -1,6 +1,8 @@
 class CreateEventParticipants < ActiveRecord::Migration[7.1]
   def change
     create_table :event_participants do |t|
+      t.references :event, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
